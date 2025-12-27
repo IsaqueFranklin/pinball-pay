c2bb6f7e1d9fac3373bf583e179aadf1fe81c90e
Isaque Franklin <isaquefrankli@gmail.com>
2025-12-26 23:05:00 -0300

    add: new index

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="w-full min-h-screen flex items-center justify-center text-center">
+    <div class="border rounded-2xl p-8 max-w-6xl mx-auto my-auto space-y-4">
+        <h1 class="text-3xl font-semibold text-center w-full">Pinball Pay</h1>
+        <p class="text-gray-500 text-md">The ArkadeOS powered Ark paywall for the internet.</p>
+    </div>
+</div>
